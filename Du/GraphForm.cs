e6c90234dbb009e1ab5e8dc0455b2577fcdc9ce5
CC Liu <Y-Y-Liu@hotmail.com>
2017-12-30 22:33:53 +0800

    将图部分修改为可以灵活建立矩阵的机制

--- Du/GraphForm.cs
+++ Du/GraphForm.cs
@@ -14,7 +14,7 @@ namespace Du
     public partial class GraphForm : Form
     {
         GraphClass gl = new GraphClass();
-        int[,] a = new int[5, 5];
+        int[,] a =new int[5,5];
         
         public GraphForm()
         {
@@ -36,35 +36,105 @@ namespace Du
                 Fname += str[i];
             }
 
-            LoadFile(Fname);
+            a = LoadFile(Fname);
             
         }
 
-        private void LoadFile(string Fname)
+        private int[,] LoadFile(string Fname)
         {
             if (File.Exists(Fname))
             {
+                int n = 0;
+                int m = 0;
                 FileStream fs = File.OpenRead(Fname);
                 BinaryReader br = new BinaryReader(fs, Encoding.Default);
                 fs.Seek(0, SeekOrigin.Begin);
-                //int[,] b=new int[5, 5] ;
-                int[] c = new int[25];
+                int[] c = new int[n];
                 string str = br.ReadString();
                 string[] btstr = str.Split(',');
+                n = Choose1(btstr.Length);
+                m = Choose2(btstr.Length);
+                int[,] a = new int[m, m];
                 c = Array.ConvertAll<string, int>(btstr, s => int.Parse(s));
-                for (int i = 0; i < 25; i++)
+                for (int i = 0; i < n; i++)
                 {
-                    a[i / 5, i % 5] = c[i];
+                    a[i / m, i % m] = c[i];
                 }
+                return a;
+            }
+            else
+            {
+                int[,] a = new int[0, 0];
+                return a;
             }
             
         }
 
-       
+        private int Choose1(int length)
+        {
+            int n = 0;
+            if (length == 0)
+            {
+                n = 0;
+            }
+            else if (length >= 1 && length < 4)
+            {
+                n = 1;
+            }
+            else if (length >= 4 && length < 9)
+            {
+                n = 4;
+            }
+            else if (length >= 9 && length < 16)
+            {
+                n = 9;
+            }
+            else if (length >= 16 && length < 25)
+            {
+                n = 16;
+            }
+            else if (length >= 25 && length < 36)
+            {
+                n = 25;
+            }
+            return n;
+        }
+
+        private int Choose2(int length)
+        {
+            int m = 0;
+            if (length == 0)
+            {
+                m = 0;
+            }
+            else if (length >= 1 && length < 4)
+            {
+                m = 1;
+            }
+            else if (length >= 4 && length < 9)
+            {
+                m = 2;
+            }
+            else if (length >= 9 && length < 16)
+            {
+                m = 3;
+            }
+            else if (length >= 16 && length < 25)
+            {
+                m = 4;
+            }
+            else if (length >= 25 && length < 36)
+            {
+                m = 5;
+            }
+            return m;
+        }
 
         private void CreateBtn_Click(object sender, EventArgs e)
         {
-            int n = 5,en = 6;
+            int n, en;
+            n = Choose2(a.Length);
+            en = n + 1;
             gl.CreateMGraph(n,en,a);
             infolabel.Text = "建立完毕";
 
